ead0cf72bd32153bad2b28b39bb5ca28ea92fe92
sendnerd <gunho.cho@sendbird.com>
2022-06-24 15:10:22 +0900

    05 frontend - pages/products/index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,20 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import * as Realm from "realm-web"
+import Header from "../components/Header"
+import Container from "../components/Container"
+import Hero from "../components/Hero"
+import Category from "../components/Category"
+import Products from "../components/Products"
+import Pagination from "../components/Pagination"
+import Footer from "../components/Footer"
 
 const Home: NextPage = () => {
   const [products, setProducts] = useState([])
 
   useEffect(() => {
     const asyncEffect = async () => {
-      const REALM_APP_ID = "application-0-mvksw"
+      const REALM_APP_ID = process.env.NEXT_PUBLIC_REALM_APP_ID
       const app = new Realm.App({id: REALM_APP_ID})
       const credentials = Realm.Credentials.anonymous()
       try {
@@ -30,11 +37,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        {products && products.map((product: any) => {
-          return <p key={product._id}>{product.name}</p>
-        })}
-      </main>
+      <div className='bg-white w-full min-h-screen'>
+        <Header />
+        <Container>
+          <Hero/>
+          <Category
+            category="Tech Wear"
+            categoryCount={`${products.length} Products`}
+          />
+          <Products products={products} />
+          <Pagination />
+        </Container>
+        <Footer />
+      </div>
     </div>
   )
 }
